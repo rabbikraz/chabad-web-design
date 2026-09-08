dc3f7e7f78bed6c860a6bc70a5d5f779f492b163
rabbikraz <rabbikraz1@gmail.com>
2026-09-07 21:52:24 -0400

    Hide CMS page title on 7407611 and 7407897

--- build/build.js
+++ build/build.js
@@ -194,7 +194,7 @@ async function build() {
   // Pages whose CMS title is hidden (style.css html.sb-notitle). Same list
   // as NOTITLE_PAGES in script.js; window.SB_NOTITLE_PAGES overrides.
   const noTitleFlag =
-    "var N=window.SB_NOTITLE_PAGES||['7080118','7474052','7437612'];" +
+    "var N=window.SB_NOTITLE_PAGES||['7080118','7474052','7437612','7407611','7407897'];" +
     "for(var j=0;j<N.length;j++){if(location.href.indexOf(N[j])>-1){document.documentElement.className+=' sb-notitle';break;}}";
   const bootJs = resolveVer + hhFlag + noTitleFlag +
     "document.write('<link rel=\"stylesheet\" href=\"'+B+'site.css\">');" +
